ef506332198786df15d2beca3f8a61fbbd1652ae
MatyPerezz <perezabrahammatias@gmail.com>
2025-11-11 08:29:18 -0300

    Update check_D.sh

--- 202406/_checks/check_D.sh
+++ 202406/_checks/check_D.sh
@@ -2,7 +2,6 @@
 TIMESTAMP=$(date +%Y%m%d)
 PUNTO="D"
 
-# ---
 clear
 echo "Ansible: "
 cat ansible/roles/2do_parcial/tasks/main.yml
@@ -10,7 +9,6 @@ echo
 cat ansible/roles/2do_parcial/templates/*
 echo
 
-# --- Comandos usados:
 echo
 echo "cat $HOME/RTA_Examen_${TIMESTAMP}/Punto_${PUNTO}.sh"
 echo
